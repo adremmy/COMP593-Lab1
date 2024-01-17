163695212686ca84c9b3273fd803dca9d16a79f4
adremmy <remilekun.adepetun@flemingcollege.ca>
2024-01-17 16:25:45 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py.
    -Added two print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines from Zen of python
 print("Errors should never pass silently.")
-print("Now is better than never.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # print lines from Zen of python
 print("Special cases aren't special enough to break the rules.")
 print("If the implementation is hard to explain, it's a bad idea.")
+print("Namespaces are one honking great idea -- let's do more of those!")
