583e50ba1fb099de7ac7685288c9ea5c937b4743
adremmy <remilekun.adepetun@flemingcollege.ca>
2024-01-17 15:11:13 -0500

    Created script files
    Added code that prints lines from Zen of python.

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Errors should never pass silently.")
+print("Now is better than never.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("Special cases aren't special enough to break the rules.")
+print("If the implementation is hard to explain, it's a bad idea.")
