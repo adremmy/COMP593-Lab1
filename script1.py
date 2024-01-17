b606838d5784a2b6f5690f1083686539ece009db
adremmy <remilekun.adepetun@flemingcollege.ca>
2024-01-17 15:40:16 -0500

    Revert "Modified both script files"
    This reverts commit e0e52a63b59c66f1c8b23e9785452ab79cadfb91.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # print lines from Zen of python
 print("Errors should never pass silently.")
+print("Now is better than never.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# print lines from Zen of python
 print("Special cases aren't special enough to break the rules.")
 print("If the implementation is hard to explain, it's a bad idea.")
